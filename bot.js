642043bc88512c941e0f5b0c68ab892916a6f79e
thienbinhdinh98 <thienbinhdinh98@gmail.com>
2021-05-16 04:06:22 -0500

    set up

---
+++ bot.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const Discord  = require('discord.js');
+const client = new Discord.Client({
+    partials: ["MESSAGE"]
+});
+
+const BOT_PREFIX = '!';
+const MOD_ME_COMMAND = 'mod-me';
+
+client.on('ready', ()=> {
+    console.log('Logged in and ready to fire');
+});
+
+function mod_user(member){
+    member.roles.add(process.env.MOD_ID);
+}
+
+client.on('message', msg =>{
+    if(msg.content === '!ping'){
+        msg.channel.send("it's Binh")
+        //msg.reply('Pong!');
+    }
+});
+
+client.on('message', msg =>{
+    if(msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`){
+        mod_user(msg.member);
+    }
+
+    if(msg.content == 'I love you bot'){
+        msg.reply("I love you too ❤️");
+        msg.react("❤️");
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
