064093376b3d4ef9267ad65b490237318d5b5dfe
thienbinhdinh98 <thienbinhdinh98@gmail.com>
2021-06-01 23:18:17 -0500

    Getting started again, trying to fetch RIOT API

--- bot.js
+++ bot.js
@@ -5,7 +5,9 @@ const client = new Discord.Client();
 
 const BOT_PREFIX = '!';
 const MOD_ME_COMMAND = 'mod-me';
-
+const FIND_USER = 'find';
+const league_user_name = '';
+ 
 client.on('ready', ()=> {
     console.log('Logged in and ready to fire');
 });
@@ -17,19 +19,26 @@ function mod_user(member){
 
 
 client.on('message', msg =>{
-    if(msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`){
-        mod_user(msg.member);
-    }
-
+    //just a fun function to do here
     if(msg.content == 'I love you bot'){
         msg.reply("I love you too ❤️");
         msg.react("❤️");
     }
 
-    if(msg.content === '!ping'){
-        msg.channel.send("it's Binh")
-        //msg.reply('Pong!');
+    //splitting the message content from the user
+    let user_message = msg.content.split(' ');
+    if(user_message[0] === `${BOT_PREFIX}${FIND_USER}`){
+        let league_user_name =  user_message[1];
+        let league_api_SUMMONERV4_url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${league_user_name}?api_key=${process.env.RIOT_TOKEN}`;
+        let response = fetch(league_api_SUMMONERV4_url);
+        let data = response.json();
+        let {name, summonerLevel} = data;
+        msg.reply(name);
+        msg.reply(summonerLevel);
     }
+    
+    
+
 });
 
 client.login(process.env.BOT_TOKEN);
